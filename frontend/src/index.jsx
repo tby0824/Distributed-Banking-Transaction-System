b62a74736ff1b6153d159e6edcb95404b97f5dcd
Yiming-jojo <145294932+Yiming-jojo@users.noreply.github.com>
2025-03-30 15:54:54 -0400

    Change node

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Updated import
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AppProvider } from './store';
-import '@fontsource/inter';
+import '@fontsource/inter'; // Add this line
 
 const container = document.getElementById('root');
-const root = createRoot(container); // Create a root
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
