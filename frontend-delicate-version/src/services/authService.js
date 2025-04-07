f5b51bb13c0cb21513830d4d985262b4c0219812
Yiming-jojo <145294932+Yiming-jojo@users.noreply.github.com>
2025-04-07 00:01:59 -0400

    Move file

--- frontend-delicate-version/src/services/authService.js
+++
@@ -1,9 +0,0 @@
-export const login = (email, password) => {
-  console.log('Login:', { email, password });
-  return { token: 'mock-token' }; // Mock response
-};
-
-export const register = (email, password) => {
-  console.log('Register:', { email, password });
-  return { token: 'mock-token' }; // Mock response
-};
--- frontend-delicate-version/src/utils/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:3000', // Mock for now, backend later
-});
-
-export default api;
